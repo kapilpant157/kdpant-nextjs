d61ad6a6bed7908170c197aee1fdbe00f3a41417
sarozpradhan64 <sarozpradhan64@gmail.com>
2022-06-22 20:30:15 +0545

    lkl

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -10,7 +10,7 @@ const Blogdetail = () => {
 //   console.log("working");  
   const router = useRouter();
     const slug = router.query.slug;    //website url
-    const blog = Bloglist.find(function(a){
+    var blog = Bloglist.find(function(a){
     
         return a.slug == slug
       
@@ -23,14 +23,14 @@ const Blogdetail = () => {
     <div className='home'>
     <div className="singlePost" key={blog.id}>
     <div className="singlePostWrapper">
-    <Image src="/shivapuri.jpg" alt={blog.alter} height={550} width={920}/>
+    <Image src="/shivapuri.jpg" alt="kapil" height={550} width={920}/>
       <h1 className="singlePostTitle">
         {blog.Heading}
         <div className="singlePostEdit">
           <i className="singlePostIcon far fa-edit"></i>
           <i className="singlePostIcon far fa-trash-alt"></i>
         </div> 
-        
+
       </h1>
       <div className="singlePostInfo">
         <span>
