0e99c346efe3383cf8b7c0cf01531e3dbab4b291
kapilpant157 <kapilpant157@gmail.com>
2022-06-22 17:14:10 +0545

    done-gallery

--- components/Bloglist.js
+++ components/Bloglist.js
@@ -2,15 +2,51 @@ const Bloglist = [
     {
         id: 1,
         category: "travel", 
-        Heading: "A day hike to shi National park.", 
-        image: "/gump.jpg",
+        Heading: "Hiking in SHivapuri National park.", 
+        image: "/shivapuri.jpg",
         alter:"first blog", 
         author_image: "/gump.jpg", 
         author: "Kapil pant", 
         date: " march 2", 
-        description: "this is blog 1 live aslkdjas lakjsdk aj sdl asdldjl askdjin Lalitpur district. I came from dolakha, charikot. I love one girl very much. Her name is sita. Thank you very much ",
+        description: "This is blog 1 live aslkdjas lakjsdk aj sdl asdldjl askdjin Lalitpur district. I came from dolakha, charikot. I love one girl very much. Her name is sita. Thank you very much ",
         slug:"a-day-hike-to-shivapuri-National-park"
     },
+    {
+        id: 2,
+        category: "Drawing", 
+        Heading: "how to draw simple things?", 
+        image: "/samir.png",
+        alter:"drawing blog", 
+        author_image: "/gump.jpg", 
+        author: "Samir Pradhan", 
+        date: "june 2", 
+        description: "This is blog 2 , written by samir live aslkdjas lakjsdk aj sdl asdldjl askdjin Lalitpur district. I came from dolakha, charikot. I love one girl very much. Her name is sita. Thank you very much ",
+        slug:"how-to-draw-simple-things"
+    },
+    {
+        id: 3,
+        category: "Coding", 
+        Heading: "how to write hello worlds in code?", 
+        image: "/samir.png",
+        alter:"drawing asddblog", 
+        author_image: "/gump.jpg", 
+        author: "Samir Pradhan", 
+        date: "june 2", 
+        description: "This is blog 2 , written by samir live aslkdjas lakjsdk aj sdl asdldjl askdjin Lalitpur district. I came from dolakha, charikot. I love one girl very much. Her name is sita. Thank you very much ",
+        slug:"how-to-write-code-in"
+    },
+    {
+        id: 4,
+        category: "Codinasdg", 
+        Heading: "how toasd write hello worlds in code?", 
+        image: "/samir.png",
+        alter:"drawing asasdddblog", 
+        author_image: "/gump.jpg", 
+        author: "Samir Pasdaaradhan", 
+        date: "junasdasde 2", 
+        description: "This asda blog 2 , written by samir live aslkdjas lakjsdk aj sdl asdldjl askdjin Lalitpur district. I came from dolakha, charikot. I love one girl very much. Her name is sita. Thank you very much ",
+        slug:"how-to-asd-code-in"
+    }
    
 ]
 export default Bloglist;  
