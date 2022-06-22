02960422e0e0584d721e00e068e1c7c218c213e8
kapilpant157 <kapilpant157@gmail.com>
2022-06-22 11:33:59 +0545

    projects

---
+++ components/Projectlist.js
@@ -0,0 +1,90 @@
+const images = [
+    {
+      id: 1,
+      imageName: "/ca.jpg",
+      tag: "Completed Projects",
+      title: "R Pandey and Associates",
+      type: "Charted Accountant",
+      caption:
+        "R. Pandey & Associates is a leading Accounting and Consulting Firm of Nepal offering a wide range of services in the field of Audit, Risk Advisory, Tax Advisory, M&A Advisory and Managed Services for National and Multinational Companies of all sizes.",
+      link: "https://www.rpanepal.com.np/",
+    },
+    {
+      id: 2,
+      imageName: "/gyanhub.jpg",
+      tag: "Ongoing Projects",
+      title: "Gyann Hub",
+      type: "Educational",
+      caption:
+        "Gyannhub is educational portal, established for distribution of notes prepared by students and teachers of various levels",
+      link: "https://gyannhub.com/",
+    },
+    {
+      id: 3,
+      imageName: "/mayapp.jpg",
+      tag: "Completed Projects",
+      title: "Maya Party Palace",
+      type: "Party Venue",
+      caption:
+        "Maya party palace is best known for its outstanding peaceful environment, excellent staff service and best management. With the very best food items and taste that makes everyone think to get more celebrations here.",
+      link: "https://dreamy-noyce-adaddb.netlify.app",
+    },
+    {
+      id: 4,
+      imageName: "/tgn.jpg",
+      tag: "Completed Projects",
+      title: "Travel Green Nepal",
+      type: "Travelling",
+      caption:
+        "Taking various travelling spots in nepal, TGN is established in order to probide informaton about tourism potential places in Nepal. we soon are planning on organizing events for booking and exploring various parts of NEPAL",
+      link: "https://travelgreennepal.com",
+    },
+    {
+      id: 5,
+      imageName: "/kapildevpant-com-np.jpg",
+      tag: "Completed Projects",
+      title: "Map of Nepal",
+      type: "Districts",
+      caption: "Personal Domain listing all districts of nepal",
+      link: "https://kapildevpant.com.np",
+    },
+    {
+      id: 6,
+      imageName: "/construction.jpg",
+      tag: "Completed Projects",
+      title: "Construction Company",
+      type: "Construction",
+      caption:
+        "ON Sale!!! ON Sale!!! ON Sale!!! , project is suitable for Construction company, Build on Reactjs makes it super fast and fully responsice. If interested in buying in affordable price, you know what to do, just send a message. ",
+      link: "https://youtu.be/f3Qv4qa1w0A",
+    },
+    {
+      id: 4,
+      imageName: "/tgn.jpg",
+      tag: "Completed Projects",
+      title: "Travel Green Nepal",
+      type: "Travelling",
+      caption:
+        "Taking various travelling spots in nepal, TGN is established in order to probide informaton about tourism potential places in Nepal. we soon are planning on organizing events for booking and exploring various parts of NEPAL",
+      link: "https://travelgreennepal.com",
+    },
+    {
+      id: 5,
+      imageName: "/kapildevpant-com-np.jpg",
+      tag: "Completed Projects",
+      title: "Map of Nepal",
+      type: "Districts",
+      caption: "Personal Domain listing all districts of nepal",
+      link: "https://kapildevpant.com.np",
+    },
+    {
+      id: 6,
+      imageName: "/construction.jpg",
+      tag: "Completed Projects",
+      title: "Construction Company",
+      type: "Construction",
+      caption:
+        "ON Sale!!! ON Sale!!! ON Sale!!! , project is suitable for Construction company, Build on Reactjs makes it super fast and fully responsice. If interested in buying in affordable price, you know what to do, just send a message. ",
+      link: "https://youtu.be/f3Qv4qa1w0A",
+    },
+  ];
