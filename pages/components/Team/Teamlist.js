c88e4605af5b765da2e35957686e81516a027a57
kapilpant157 <kapilpant157@gmail.com>
2022-06-22 10:22:42 +0545

    team

---
+++ pages/components/Team/Teamlist.js
@@ -0,0 +1,27 @@
+const Teamlist = [
+    {
+        id: 1,
+        image: "/ran-7.jpg",
+        alt:"Kapil Dev Pant",
+        name:"Kapil Dev Pant",
+        description: "Graphic designer",
+        link:"www.kapilpant157.com.np"
+    },
+    {
+        id: 2,
+        image: "/Saroj.jpg",
+        alt:"Friend of Kapil pant",
+        name:"Saroj Pradhan",
+        description: "Front-End Designer",
+        link:"www.pradhansaroj.com.np"
+    },
+    {
+        id: 3,
+        image: "/Sudip.jpg",
+        alt:"Friend of kapil dev pant",
+        name:"Sudip Wod",
+        description: "Back-ed developer",
+        link:"www.sudipwod.com.np"
+    }
+]
+export default Teamlist;
