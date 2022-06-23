3e89e4c9fa6bdc02c9824efdfdb1c0a96e477c63
kapilpant157 <kapilpant157@gmail.com>
2022-06-23 15:57:36 +0545

    as

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -4,3 +4,18 @@ import Navbartwo from "../../components/Navbar/Navbartwo";
 import Sidebar from "../../components/Sidebar";
 import Singleblog from "../../components/Singleblog";
 
+
+const Blogdetail = () => {
+
+  return (
+    <>
+      <Navbartwo />
+      <div className="home">
+        <Singleblog />
+        <Sidebar />
+      </div>
+    </>
+  );
+};
+
+export default Blogdetail;
