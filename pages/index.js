b4875a18a0155b8c9c91d544b07ad6f886f63e36
Kapil Dev Pant <40812026+kapilpant157@users.noreply.github.com>
2023-04-26 14:09:36 +0530

    g

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ export default function Home() {
         <link rel="icon" href="/sign.ico" />
       </Head>
       {/* <Header/> */}
-      <Aboutme/> 
-      {/*<Works/>*/}
-      {/* <Myteam/> */}
+      {/*<Aboutme/> */}
+    <Works/>
+      <Myteam/>
       {/* <Contact/> */}
       <Footer/>
       <Navbar/>
